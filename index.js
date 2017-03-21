d46a583dfecd8eeec4289920350bb21a883cf924
André "Intentor" Martins <andre@intentor.com.br>
2017-03-20 21:25:59 -0300

    Add database connection test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 8080));
@@ -10,11 +11,22 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err) { 
+        console.error(err); response.send("Error " + err); 
+      } else {
+        response.render('pages/index', {results: result.rows} );
+      }
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+app.get('/db', function (request, response) {
+  
+});
