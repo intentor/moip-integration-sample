5765570a4da15892598e790a3c0cb154361a1ba3
André "Intentor" Martins <andre@intentor.com.br>
2017-03-20 21:48:46 -0300

    Update database table command

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM products', function(err, result) {
       done();
       if (err) { 
         console.error(err); response.send("Error " + err); 
