aa7b1a2e0b19435166aa8df361f9b1a29234fc0d
André "Intentor" Martins <andre@intentor.com.br>
2017-03-22 23:49:29 -0300

    Add basic Moip integration

---
+++ config.js
@@ -0,0 +1,11 @@
+/* App settings */
+
+let config = {
+    moip: {
+        token: 'WRX3KHMBTCNGLABXSHFQT2LB68X9WQKO',
+        key: 'VHDNDCDZEKRLO1M5K5VIEQBCDSV4G5JBK2ZZYMJA',
+        url: 'sandbox.moip.com.br',
+    }
+};
+
+exports.config = config;
--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,8 @@
 /* Order controller. */
 
+var config = require('../config.js');
+var moipOrder = require('../lib/moip/moipOrder.js');
+var moipPayment = require('../lib/moip/moipPayment.js');
 var clientDao = require('../service/dao/clientDao.js');
 var orderDao = require('../service/dao/orderDao.js');
 var productDao = require('../service/dao/productDao.js');
@@ -16,7 +19,7 @@ exports.get = function(request, response) {
     setTimeout(function() {
         response.contentType('application/json');
         response.end('{ "orderId": 666, "sent": ' + request.body.orderId + ', "status": 1, "message": "" }');
-    }, 5000);
+    }, 2000);
 };
 
 /**
@@ -38,7 +41,7 @@ exports.put = function(request, response) {
  * @param {object} response Response parameters.
  */
 function createClient(request, response) {
-    var data = request.body.data;
+    let data = request.body.data;
 
     clientDao.createOrUpdate({
         email: data[0].value,
@@ -64,8 +67,8 @@ function createOrder(clientId, request, response) {
     // On production environment, the data must be validated, e.g. by using the product's IDs
     // to load them from database and recalculating prices, discounts and interest.
 
-    var cart = request.body.cart;
-    var data = request.body.data;
+    let cart = request.body.cart;
+    let data = request.body.data;
 
     orderDao.create({
         clientId: clientId,
@@ -75,7 +78,86 @@ function createOrder(clientId, request, response) {
         discountCode: data[9].value,
         products: cart.products
     }, function(orderId) {
-        response.contentType('application/json');
-        response.end('{ "orderId": ' + orderId + ' }');
+        sendOrder(orderId, clientId, request, response);
     });
+}
+
+/**
+ * Send the order to the Moip server.
+ * 
+ * @param {int} orderId Internal order ID.
+ * @param {int} clientId Client ID.
+ * @param {object} request Request parameters.
+ * @param {object} response Response parameters.
+ */
+function sendOrder(orderId, clientId, request, response) {
+    let order = new moipOrder.MoipOrder(config.config.moip);
+    let data = request.body.data;
+    let products = request.body.cart.products;
+
+    for (let index = 0; index < products.length; index++) {
+        let product = products[index];
+
+        order.addItem({
+            product: product.name,
+            quantity: product.quantity,
+            detail: '-',
+            price: product.price,
+        });
+    }
+
+    order.setCustomer({
+        ownId: clientId,
+        fullname: data[1].value, 
+        email: data[0].value,
+    })
+    .execute().then(details => {
+        console.log('order.execute', details);
+        sendPayment(orderId, order, request, response);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+/**
+ * Send the order to the Moip server.
+ * 
+ * @param {int} orderId Internal order ID.
+ * @param {object} order Order object.
+ * @param {object} request Request parameters.
+ * @param {object} response Response parameters.
+ */
+function sendPayment(orderId, order, request, response) {
+    let payment = new moipPayment.MoipPayment(order);
+    let data = request.body.data;
+    let phone = data[4].value.split(' ');
+    let expiration = data[7].value.split('/');
+
+    payment.setInstallments(data[8].value)
+        .setCreditCard({
+            number: data[5].value,
+            expirationMonth: expiration[0],
+            expirationYear: expiration[1],
+            cvc: data[6].value
+        })
+        .setHolder({
+            fullname: data[1].value,
+            birthdate: data[2].value
+        })
+        .setDocument(data[3].value)
+        .setPhone({
+            countryCode: phone[0],
+            areaCode: phone[1],
+            number:  phone[2].replace('-', ''),
+        })
+        .execute().then(details => {
+            console.log('payment.execute', details);
+
+            response.contentType('application/json');
+            response.end('{ "orderId": ' + orderId + ' }');
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
